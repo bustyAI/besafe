f9bb563bccd29d7ccbe752364341c2d197da7cd2
Oscar Ochoa <97921525+bustyAI@users.noreply.github.com>
2023-11-01 18:52:53 -0700

    Adding react router libraries

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Navbar } from '@/Components'
+
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
-      <Navbar/>
+      <Navbar />
+
     </main>
   )
 }
